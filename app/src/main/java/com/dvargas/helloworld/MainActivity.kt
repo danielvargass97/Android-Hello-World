2d531e07505525ea66b80a235d1d1fda52da6b2b
danielvargass97 <daniel_vrgs@hotmail.com>
2023-02-16 16:57:23 -0500

    Codelab complete

---
+++ app/src/main/java/com/dvargas/helloworld/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.dvargas.helloworld
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("MainActivity", "Hello World");
+    }
+}
